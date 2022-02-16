88e2833aaa4d02d85f514408b3dc914f41b9dcc1
d92240 <maximilian.hoeschler@d-fine.de>
2022-02-16 17:20:47 +0100

    Preparations to merge into master

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --env-file run_config.txt --mount type=bind,source=C:\\Maximilian_Hoeschler\\Projekte\\04_PlanQK\\Repo\\QuantumClassifierDocker\\model,target=/quantum-anomaly/model qanomaly:1.1
+docker run --env-file run_config.txt --mount type=bind,source=C:\\path\\to\\repo\\model,target=/quantum-anomaly/model qanomaly:1.1
