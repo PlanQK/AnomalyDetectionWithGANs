f398ea946d717b3316e4f2ab2c780e4f6cf8226c
Lucila Zarate <Lucila.Zarate@d-fine.de>
2022-04-26 17:15:03 +0200

    Adapt run file to use json files instead of env variables
    To avoid the limit on the size of the env variables, the new template
    image allows to provide data and params via json files.

--- run.sh
+++ run.sh
@@ -2,17 +2,18 @@
 
 git_root=`git rev-parse --show-toplevel`
 path_prefix="input/"
-path_to_json="small_input_test"
+path_to_json="train"
 
 file="${git_root}/${path_prefix}${path_to_json}.json"
 
 echo "using ${file} as input"
 
-data=`cat $file | jq '.data' | base64`
-params=`cat $file | jq '.params' | base64`
-
-echo "parameters: ${params}"
-echo "data: ${data}"
+cat $file | jq '.data' > $git_root/input/data.json
+cat $file | jq '.params' > $git_root/input/params.json
 
 docker rm -f qgan
-docker run -it -e INPUT_DATA="${data}" -e INPUT_PARAMS="${params}" --name qgan planqk-service
+docker run -it \
+  -e BASE64_ENCODED=false \
+  -v $git_root/input/data.json:/var/input/data/data.json \
+  -v $git_root/input/params.json:/var/input/params/params.json \
+  --name qgan planqk-service
