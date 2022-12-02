e6dc4afe043367b8b1e3cc03cb6cc08313f543e2
Hendrik Heine <hendrikheine@gmail.com>
2022-12-02 19:59:46 +0100

    Add and change utilities docstrings

--- libs/utilities.py
+++ libs/utilities.py
@@ -8,23 +8,33 @@ import numpy as np
 
 
 class NpEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        if isinstance(obj, np.floating):
-            return float(obj)
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return super(NpEncoder, self).default(obj)
+    """Class that extends the JSONEncoder to work with numpy objects
+    by converting them to their standard counterparts.
+    """    
+    def default(self, o):
+        if isinstance(o, np.integer):
+            return int(o)
+        if isinstance(o, np.floating):
+            return float(o)
+        if isinstance(o, np.ndarray):
+            return o.tolist()
+        return super(NpEncoder, self).default(o)
 
 
 def reformat_for_json(data):
-    """Outputs data to json file """
+    """Transform data into a json compatible format.
+    """    
     # create a json compatible dictionary
     return json.loads(json.dumps({**data}, cls=NpEncoder))
 
 
 def export_to_json(data, fp=None):
+    """Writes json data to file.
+
+    Args:
+        data: The data to write
+        fp (optional): The file path. If none specified, writes to model/data.json.
+    """
     if not fp:
         fp = "model/data.json"
     with open(fp, "w", encoding='utf-8') as file:
